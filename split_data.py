01f2a42529698a0703e60ef55b43bfb41d37c04a
Timur Kakhanovich <timkakhanovich@gmail.com>
2023-04-10 15:22:54 +0200

    Improve code structure from the old one
    * replace os.path with pathlib
    * upgrade readability of the code
    * reorganized structure of the code
    * remove unnecessary logic from code

--- split_data.py
+++ split_data.py
@@ -1,30 +1,37 @@
-import os
+from pathlib import Path
 import cv2
 import numpy as np
 
-def load_data_to_files(data_dir, name):
-    folder = os.path.join(data_dir, name)
-    files = os.listdir(folder)
+
+def load_data_to_files(data_dir: Path, name: str) -> None:
+    source_data_folder = data_dir / name
+    target_data_folder = Path("VGG-Face2-Data/")
+    files = source_data_folder.glob("**/*")
 
     train, val, test = np.split(files, [int(0.8 * len(files)), int(0.9 * len(files))])
+    
+    train_target_folder = target_data_folder / train / name
+    test_target_folder = target_data_folder / test / name
+    val_target_folder = target_data_folder / val / name
 
-    os.mkdir('VGG-Face2-Data/train/' + name)
+    train_target_folder.mkdir(parents=True, exist_ok=True)
     for jpg in train:
-        img = cv2.imread(folder + '/' + jpg)
-        cv2.imwrite('VGG-Face2-Data/train/' + name + '/' + jpg, img)
+        img = cv2.imread(source_data_folder / jpg)
+        cv2.imwrite(train_target_folder / jpg, img)
 
-    os.mkdir('VGG-Face2-Data/test/' + name)
+    test_target_folder.mkdir(parents=True, exist_ok=True)
     for jpg in test:
-        img = cv2.imread(folder + '/' + jpg)
-        cv2.imwrite('VGG-Face2-Data/test/' + name + '/' + jpg, img)
+        img = cv2.imread(source_data_folder / jpg)
+        cv2.imwrite(test_target_folder / jpg, img)
     
-    os.mkdir('VGG-Face2-Data/val/' + name)
+    val_target_folder.mkdir(parents=True, exist_ok=True)
     for jpg in val:
-        img = cv2.imread(folder + '/' + jpg)
-        cv2.imwrite('VGG-Face2-Data/val/' + name + '/' + jpg, img)
+        img = cv2.imread(source_data_folder / jpg)
+        cv2.imwrite(val_target_folder / jpg, img)
 
-if __name__ == "__main__":
-    #os.chdir(r'E:\\Projects\\CourseWork\\face_mask_recognition')
-    
-    for pers in os.listdir('VGG-Face2'):
-        load_data_to_files('VGG-Face2/', pers)
+
+if __name__ == "__main__":    
+    source_data_path = Path("VGG-Face2")
+
+    for pers in source_data_path.glob("**/*"):
+        load_data_to_files(source_data_path, pers)
